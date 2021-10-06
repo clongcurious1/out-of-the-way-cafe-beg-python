3bba83ce267a8dada39da426ee162e28c3b8d254
clong <clong389@gmail.com>
2021-10-06 19:43:17 -0400

    wrote print statements for these sections: Welcome, About the Appalachian Trail, About the Out of the Way Cafe

--- main.py
+++ main.py
@@ -1,20 +1,93 @@
+#Imports
 import math
+import time
+
+#Variables
+
+#Functions
+
+#Other situations
 
 #Introduction
+print("***WELCOME ADVENTURER!***")
+time.sleep(1)
+print('Congratulations! You have decided to hike the Appalachian Trail.')
+time.sleep(3)
 #About the Appalachian Trail
+print('***ABOUT YOUR QUEST***')
+time.sleep(2)
+print('The Appalachian Trail (AT) stretches approximately 2200 miles from Georgia to Maine.')
+time.sleep(3)
+print('Approximately 3,000 people visit sections of the trail each year.')
+time.sleep(3)
+print ('The most avid of these outdoor enthusiasts are known as THRU HIKERS.')
+time.sleep(3)
+print('You, my friend, are a Thru Hiker!!!')
+time.sleep(3)
+print('You are determined to hike through all FOURTEEN states.')
+time.sleep(3)
+print('Thru Hiking is not for the faint of heart.')
+time.sleep(3)
+print('In fact, the AT Thru Hiking distance equals scaling Mt. Everest 16 TIMES!')
+time.sleep(4)
+print('Less than 25 percent of people who set out to hike the entire AT actually achieve their goal.')
+time.sleep(4)
+#About the Hiker
+print("***WHERE ARE YOU TODAY?***")
+time.sleep(3)
+print("We've caught up with you near the highest point on the trail, Clingmans Dome.")
+time.sleep(3)
+print('Elevation 6643 ft.')
+time.sleep(3)
+print('YAWN...You reached the Great Smoky Mountains National Park LATE last night.')
+time.sleep(2)
+print('The nearest major city is Gatlinburg, TN.')
+time.sleep(2)
 #About the Out of the Way Cafe
-#Take the drink order
+print("***FUELING THE MACHINE***")
+time.sleep(2)
+#About the Out of the Way Cafe
+print('Breakfast is the most important meal of the day.')
+time.sleep(2)
+print("You've decided to eat at the OUT OF THE WAY Café.")
+time.sleep(2)
+print('Your stomach is growling LOUDLY.')
+time.sleep(2)
+print("You're so hungry YOU COULD EAT A BEAR.")
+time.sleep(2)
+print('You walk in and take a seat at the counter.')
+time.sleep(2)
+print('You lean your backpack against the footrail.')
+time.sleep(2)
+print('Here comes the waitress.')
+time.sleep(2)
+#Meet Gladys
+#Server takes Hiker's drink order
+#Validate response and proceed
 #Pause Server gets drink
-#Serve drink with Turtle
+#Serve drink with Turtle 
+#REQUIREMENT: reading a file into the program
+#REQUIREMENT: 3 functions or methods, minimum 1 CALL, 1 RETURN VALUE used elsewhere
+#REQUIREMENT: visual representation of data in some form
 #You're not from around here, are you?
-#How hungry are you? Time of last meal
-#Time Zone Time of Day converter 
-#Take the food order
+#You must be starving. Share AT daily calorie and weight loss stats
+#Time of last meal
+#REQUIREMENT: Converting Values, Time Zone Time of Day converter 
+#Server takes Hiker's food order
+#Validate response and proceed
+#Server leaves
+#Hiker notes local weather looking out the window. Share Clingman rainforest precip stats.
 #Hiker checks weather back home
-#Hiker checks weather at Clingman's Dome (TN)
-#Serve food with Turtle
-#Hiker checks local Tweets or keyword Appalachian Trail
+#REQUIREMENT: Utilize data pulled from API 
+#Lost compass! Hiker inventories items stored in backpack. 
+#REQUIREMENT: Create list with multiple values, retrieve one value, use elsewhere in program
+#Serve food with Turtle 
+#While eating, Hiker checks local Tweets or keyword Appalachian Trail or Gatlinburg
+#Display Tweets in Terminal 
+#REQUIREMENT: import, read + display data from JSON file
+#REQUIREMENT: Visualize data in some fashion (WordCloud)
 #Server returns to clear plate
-#How far are you going? Will you be back this way? 
-#If YES, countdown timer and "Happy Trails"
+#REQUIREMENT: Server remarks on item from list
+#How far are you going? Share Thru-Hiker stats. Will you be back this way? 
+#If YES, REQUIREMENT: countdown timer, and "Happy Trails"
 #If NO, pay at the register and Happy Trails!
