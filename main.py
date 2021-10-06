2df83cb616c92bf09a024009a5431ac78fb529b0
clong <clong389@gmail.com>
2021-10-06 17:28:35 -0400

    wrote Comments to block each section of code to be written, highlighting where REQUIREMENTS are met

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import math
+
+#Introduction
+#About the Appalachian Trail
+#About the Out of the Way Cafe
+#Take the drink order
+#Pause Server gets drink
+#Serve drink with Turtle
+#You're not from around here, are you?
+#How hungry are you? Time of last meal
+#Time Zone Time of Day converter 
+#Take the food order
+#Hiker checks weather back home
+#Hiker checks weather at Clingman's Dome (TN)
+#Serve food with Turtle
+#Hiker checks local Tweets or keyword Appalachian Trail
+#Server returns to clear plate
+#How far are you going? Will you be back this way? 
+#If YES, countdown timer and "Happy Trails"
+#If NO, pay at the register and Happy Trails!
